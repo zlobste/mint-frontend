e2e84ac5177006ee9d555463ec5d3962b089feff
zlobste <mykola.krainiuk@nure.ua>
2020-09-13 22:20:52 +0300

    Add fetch user info. Add auth signature to request.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState, useContext, useEffect, useCallback} from 'react'
+import {useHttp} from '../hooks/http.hook'
+import {AuthContext} from '../context/AuthContext'
+import {Loader} from '../components/Loader'
+
 
 export const MainPage = () => {
-  return (
-    <div>
-      Main Page
-    </div>
-  )
+    const [user, setUser] = useState({})
+    const {loading, request} = useHttp()
+    const {token} = useContext(AuthContext)
+
+    const fetchUserInfo = useCallback(async () => {
+        try {
+            const fetched = await request('/api/user', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            })
+            setUser(fetched)
+        } catch (e) {}
+    }, [token, request])
+
+    useEffect(() => {
+        fetchUserInfo()
+    }, [fetchUserInfo])
+
+    if (loading) {
+        return <Loader/>
+    }
+
+    return (
+        <>
+        {!loading &&
+        <ul>
+            <li>id: {user.id}</li>
+            <li>email: {user.email}</li>
+        </ul>}
+        </>
+    )
 }
+
+
+
+
+
+
